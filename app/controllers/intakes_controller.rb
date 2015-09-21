7241e3f0b0e95fe37e18eb1142216c0033b05530
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-21 09:43:55 +0100

    added new intake form

--- app/controllers/intakes_controller.rb
+++ app/controllers/intakes_controller.rb
@@ -1,2 +1,16 @@
 class IntakesController < ApplicationController
+
+  def index
+    @user = User.find(params[:user_id])
+    @intakes = @user.intakes
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @intake = Intake.new
+  end
+
+  def create
+  end
+
 end
