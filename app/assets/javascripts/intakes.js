4cf6c48a4eabd605608e67d11b6d64a8541d3aff
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-22 15:50:43 +0100

    added in hidden form to intakes index

--- app/assets/javascripts/intakes.js
+++ app/assets/javascripts/intakes.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 
   button.on("click", getNewDish);
 
-  // $(".results").on("click", $(".dish-result"), createNewDish);
+  // $(".results").on("click", $(".dish-result"), newIntake);
 
   function getNewDish(e){
     console.log("i'm clicked");
@@ -47,11 +47,13 @@ $(document).ready(function(){
     newDish.appendTo(".results");
   }
 
-  // function createNewDish(e){
+  // function newIntake(e){
   //   e.preventDefault;
   //   console.log("link is clicked");
-  //   var method = "get";
   //   var $this = $(e.target.parentElement);
+  //   dishName = $this.data("dish");
+
+  //   var method = "get";
   //   var data = {
   //     dishName: $this.data("dish")
   //   };
@@ -59,28 +61,10 @@ $(document).ready(function(){
   //   request(url, method, data)
   // }
 
-  // var values = [250, 10000, 340, 290, 200, 50, 45, 85, 95, 120];
+  // function renderForm(){
+  //   form = $("<form class='new-intake-form'>");
+    
 
-  // drawChart(values, "#chart", 10);
-  
-  // function drawChart(data, selector, padding) {
-  //   var max = Math.max.apply(Math, data);
-  //   var chart = document.querySelector(selector);
-  //   var barwidth = ((chart.offsetWidth - (values.length - 1) * padding - (data.length) * 10) / data.length);
-  //   var sum = data.reduce(function(pv, cv) {
-  //     return pv + cv;
-  //   }, 0);
-  //   var left = 0;
-  //   for (var i in data) {
-  //     var newbar = document.createElement('div');
-  //     newbar.setAttribute("class", "bar");
-  //     newbar.style.width = barwidth + "px";
-  //     newbar.style.height = ((data[i] / max) * 100) + "%";
-  //     newbar.style.left = left + "px";
-  //     chart.appendChild(newbar);
-  //     left += (barwidth + padding + 10);
-  //     var text = document.createElement('p');
-  //     text.InnerHTML = "testing";newbar.appendChild(text);}
   // }
 
 })
--- app/controllers/intakes_controller.rb
+++ app/controllers/intakes_controller.rb
@@ -3,6 +3,7 @@ class IntakesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @intakes = @user.intakes
+    @intake = Intake.new
     nutrients = @user.current_intake
     @nutrient_intakes = nutrients.sort_by {|k,v| v}
   end
