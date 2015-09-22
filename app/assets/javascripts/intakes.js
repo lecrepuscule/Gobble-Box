b04fe24ed4f1ab2e4dc1f2a440787250f3612b83
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-22 16:05:00 +0100

    hidden form can now be shown upon click event

--- app/assets/javascripts/intakes.js
+++ app/assets/javascripts/intakes.js
@@ -6,10 +6,13 @@
 $(document).ready(function(){
 
   var button = $(".search-dish");
+  var submitIntakeButton = $("#intake-submit");
 
   button.on("click", getNewDish);
 
-  // $(".results").on("click", $(".dish-result"), newIntake);
+  $(".results").on("click", $(".dish-result"), newIntake);
+
+
 
   function getNewDish(e){
     console.log("i'm clicked");
@@ -40,30 +43,36 @@ $(document).ready(function(){
   }
 
   function appendDish(dish, user_id){
-    newDishTemplate = "<li class='dish-result'><a href='/users/" + user_id + "/intakes/new'>"+dish.recipe.label+"</a></li>"
+    // newDishTemplate = "<li class='dish-result'><a href='/users/" + user_id + "/intakes/new'>"+dish.recipe.label+"</a></li>"
+    newDishTemplate = "<li class='dish-result'><a href='#'>"+dish.recipe.label+"</a></li>"
     newDish = $(newDishTemplate);
     newDish.data("dish", dish.recipe.label);
     newDish.data("user_id", user_id);
     newDish.appendTo(".results");
   }
 
-  // function newIntake(e){
-  //   e.preventDefault;
-  //   console.log("link is clicked");
-  //   var $this = $(e.target.parentElement);
-  //   dishName = $this.data("dish");
+  function newIntake(e){
+    e.preventDefault;
+    console.log("link is clicked");
 
-  //   var method = "get";
-  //   var data = {
-  //     dishName: $this.data("dish")
-  //   };
-  //   var url = "/users/" + $this.data("user_id") + "/intakes/new";
-  //   request(url, method, data)
-  // }
+    var $this = $(e.target.parentElement);
+
+    $("#intake-primary").removeClass("show").addClass("hide");
+    $("#intake-secondary").removeClass("hide").addClass("show");
+
+    $("#dish-select").val($this.data("dish"));
+
+    // var method = "get";
+    // var data = {
+    //   dishName: $this.data("dish")
+    // };
+    // var url = "/users/" + $this.data("user_id") + "/intakes/new";
+    // request(url, method, data)
+  }
 
   // function renderForm(){
   //   form = $("<form class='new-intake-form'>");
-    
+
 
   // }
 
