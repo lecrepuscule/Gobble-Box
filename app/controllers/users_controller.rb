c70571b6685c194f584d23d8e2bfe891d75e14ff
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-22 23:43:56 +0100

    added sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
 
-  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to sessions_new_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :gender, :age, :weight, :height)
+  end
   
 end
